7a5a5c13c47508e1dcb5b963f91dfd2852fdb1ff
prashjet <prashinjethwa@gmail.com>
2023-11-21 13:33:23 +0100

    add code

---
+++ pysequence/analysis/compare_sequences.py
@@ -0,0 +1,23 @@
+from ..sequences import get_sequence
+
+def intersection(list1, list2):
+    """Returns a list containing elements which appear in list1 and list2"""
+    result = [value for value in list1 if value in list2]
+    return result
+
+def get_intersection_of_sequences(n, seq_name1, seq_name2):
+    """Returns a list of numbers below n appearing in two nams sequences"""
+    seq_func1 = get_sequence(seq_name1)
+    seq1 = seq_func1(n)
+    seq_func2 = get_sequence(seq_name2)
+    seq2 = seq_func2(n)
+    common_elements = intersection(seq1, seq2)
+    return common_elements
+
+if __name__ == "__main__":
+    n = 50
+    fibonnaci_primes = get_intersection_of_sequences(n, 'prime', 'fibonacci')
+    n_fibonnaci_primes = len(fibonnaci_primes)
+    print(f'There are {n_fibonnaci_primes} Fibonacci primes below {n}. These are:')
+    for p in fibonnaci_primes:
+        print(f'   {p}')
---
+++ pysequence/sequences.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def even_numbers(n):
+    """Return a list of all even numbers up to n
+    """
+    result = []
+    a = 2
+    while a < n:
+        result.append(a)
+        a = a+2
+    return result
+
+def fibonacci_numbers(n):
+    """Return a list of all Fibonnaci numbers up to n
+    """
+    result = []
+    a, b = 1, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+def prime_numbers(n):
+    """Return a list of all prime numbers up to n
+    """
+    integers = np.linspace(2, n, n-1, dtype=int)
+    seive = np.array([True for i in range(n-1)])
+    for i in range(2, n-1):
+        seive[i+i-1::i] = False
+    result = integers[seive]
+    result = list(result)
+    return result
+
+def get_sequence(name):
+    """Return the appropriate sequence function given a string name
+    """
+    if name=='even':
+        return even_numbers
+    elif name=='fibonacci':
+        return fibonacci_numbers
+    elif name=='prime':
+        return prime_numbers
+    else:
+        raise ValueError('name should be one of [even, fibonacci, prime]')
+
+if __name__ == "__main__":
+    n = 25
+    for sequence_name in ['even', 'fibonacci', 'prime']:
+        sequence_func = get_sequence(sequence_name)
+        print(f'{sequence_name} numbers below {n}: {sequence_func(n)}')
