ed7a8dbaf8c0f54618c81039fb5a6fb8ed68a00c
prashjet <prashinjethwa@gmail.com>
2024-01-17 10:09:08 +0100

    test workflow 1

--- test/test_compare_sequences.py
+++ test/test_compare_sequences.py
@@ -3,4 +3,4 @@ from pysequence.analysis import compare_sequences
 def test_compare_fib_primes():
     n = 50
     fib_primes = compare_sequences.get_intersection_of_sequences(n, 'prime', 'fibonacci')
-    assert fib_primes == [2, 3, 5, 13]
+    assert fib_primes == [2, 3, 5, 15]
